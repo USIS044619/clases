a6f22932edaf8350a34732b66a3692d18e62661c
sao <sao@DESKTOP-GHD8624>
2020-06-26 22:51:16 -0600

    prueba2

---
+++ pazluis/pazluis/Busqueda_Vehiculos.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pazluis
+{
+    public partial class Busqueda_Vehiculos : Form
+    {
+
+        conexion objConexion = new conexion();
+        public int _IdVehiculo;
+ 
+
+
+        public Busqueda_Vehiculos()
+        {
+            InitializeComponent();
+        }
+
+        private void Busqueda_Vehiculos_Load(object sender, EventArgs e)
+        {
+            GrdBusquedaVehiculo.DataSource = objConexion.obtener_datos().Tables["tbl_vehiculo"].DefaultView;
+        }
+
+        private void BtnSeleccionar_Click(object sender, EventArgs e)
+        {
+            if (GrdBusquedaVehiculo.RowCount > 0)
+            {
+                _IdVehiculo = int.Parse(GrdBusquedaVehiculo.CurrentRow.Cells["IdVehiculo"].Value.ToString());
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("NO hay datos que seleccionar", "Busqueda de Vehiculos",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        void filtrar_datos(String valor)
+        {
+            BindingSource bs = new BindingSource();
+            bs.DataSource = GrdBusquedaVehiculo.DataSource;
+            bs.Filter = "marca like '%" + valor + "%' or modelo like '%" + valor + "%' or year like '%" + valor + "%'";
+            GrdBusquedaVehiculo.DataSource = bs;
+
+        }
+
+        private void TxtBuscar_TextChanged(object sender, EventArgs e)
+        {
+            filtrar_datos(TxtBuscar.Text);
+        }
+
+        private void BtnCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
--- pazluis/pazluis/Form1.Designer.cs
+++ pazluis/pazluis/Form1.Designer.cs
@@ -28,33 +28,241 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtmarca = new System.Windows.Forms.TextBox();
+            this.txtmodelo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtyear = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pnlNavegacion = new System.Windows.Forms.Panel();
+            this.lblregistroxden = new System.Windows.Forms.Label();
+            this.btnUltimo = new System.Windows.Forms.Button();
+            this.btnSiguiente = new System.Windows.Forms.Button();
+            this.btnAnterior = new System.Windows.Forms.Button();
+            this.btnPrimero = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblidvehiculo = new System.Windows.Forms.Label();
+            this.pnlNavegacion.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // btnNuevo
             // 
-            this.button1.Location = new System.Drawing.Point(408, 238);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnNuevo.Location = new System.Drawing.Point(59, 288);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 0;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(151, 288);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 1;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(242, 288);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 2;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(335, 288);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(164, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "marca";
+            // 
+            // txtmarca
+            // 
+            this.txtmarca.Location = new System.Drawing.Point(222, 52);
+            this.txtmarca.Name = "txtmarca";
+            this.txtmarca.Size = new System.Drawing.Size(95, 20);
+            this.txtmarca.TabIndex = 5;
+            // 
+            // txtmodelo
+            // 
+            this.txtmodelo.Location = new System.Drawing.Point(222, 106);
+            this.txtmodelo.Name = "txtmodelo";
+            this.txtmodelo.Size = new System.Drawing.Size(95, 20);
+            this.txtmodelo.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(164, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "modelo";
+            // 
+            // txtyear
+            // 
+            this.txtyear.Location = new System.Drawing.Point(222, 156);
+            this.txtyear.Name = "txtyear";
+            this.txtyear.Size = new System.Drawing.Size(95, 20);
+            this.txtyear.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(164, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "año";
+            // 
+            // pnlNavegacion
+            // 
+            this.pnlNavegacion.Controls.Add(this.lblregistroxden);
+            this.pnlNavegacion.Controls.Add(this.btnUltimo);
+            this.pnlNavegacion.Controls.Add(this.btnSiguiente);
+            this.pnlNavegacion.Controls.Add(this.btnAnterior);
+            this.pnlNavegacion.Controls.Add(this.btnPrimero);
+            this.pnlNavegacion.Location = new System.Drawing.Point(151, 207);
+            this.pnlNavegacion.Name = "pnlNavegacion";
+            this.pnlNavegacion.Size = new System.Drawing.Size(191, 49);
+            this.pnlNavegacion.TabIndex = 16;
+            // 
+            // lblregistroxden
+            // 
+            this.lblregistroxden.AutoSize = true;
+            this.lblregistroxden.Location = new System.Drawing.Point(67, 16);
+            this.lblregistroxden.Name = "lblregistroxden";
+            this.lblregistroxden.Size = new System.Drawing.Size(36, 13);
+            this.lblregistroxden.TabIndex = 6;
+            this.lblregistroxden.Text = "x de n";
+            // 
+            // btnUltimo
+            // 
+            this.btnUltimo.Location = new System.Drawing.Point(156, 3);
+            this.btnUltimo.Name = "btnUltimo";
+            this.btnUltimo.Size = new System.Drawing.Size(32, 38);
+            this.btnUltimo.TabIndex = 3;
+            this.btnUltimo.Text = ">|";
+            this.btnUltimo.UseVisualStyleBackColor = true;
+            this.btnUltimo.Click += new System.EventHandler(this.btnUltimo_Click);
+            // 
+            // btnSiguiente
+            // 
+            this.btnSiguiente.Location = new System.Drawing.Point(125, 3);
+            this.btnSiguiente.Name = "btnSiguiente";
+            this.btnSiguiente.Size = new System.Drawing.Size(32, 38);
+            this.btnSiguiente.TabIndex = 2;
+            this.btnSiguiente.Text = ">";
+            this.btnSiguiente.UseVisualStyleBackColor = true;
+            this.btnSiguiente.Click += new System.EventHandler(this.btnSiguiente_Click);
+            // 
+            // btnAnterior
+            // 
+            this.btnAnterior.Location = new System.Drawing.Point(33, 3);
+            this.btnAnterior.Name = "btnAnterior";
+            this.btnAnterior.Size = new System.Drawing.Size(32, 38);
+            this.btnAnterior.TabIndex = 1;
+            this.btnAnterior.Text = "<";
+            this.btnAnterior.UseVisualStyleBackColor = true;
+            this.btnAnterior.Click += new System.EventHandler(this.btnAnterior_Click);
+            // 
+            // btnPrimero
+            // 
+            this.btnPrimero.Location = new System.Drawing.Point(2, 3);
+            this.btnPrimero.Name = "btnPrimero";
+            this.btnPrimero.Size = new System.Drawing.Size(32, 38);
+            this.btnPrimero.TabIndex = 0;
+            this.btnPrimero.Text = "|<";
+            this.btnPrimero.UseVisualStyleBackColor = true;
+            this.btnPrimero.Click += new System.EventHandler(this.btnPrimero_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(163, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "ID";
+            // 
+            // lblidvehiculo
+            // 
+            this.lblidvehiculo.AutoSize = true;
+            this.lblidvehiculo.Location = new System.Drawing.Point(219, 15);
+            this.lblidvehiculo.Name = "lblidvehiculo";
+            this.lblidvehiculo.Size = new System.Drawing.Size(0, 13);
+            this.lblidvehiculo.TabIndex = 18;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(498, 392);
+            this.Controls.Add(this.lblidvehiculo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pnlNavegacion);
+            this.Controls.Add(this.txtyear);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtmodelo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtmarca);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnNuevo);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.pnlNavegacion.ResumeLayout(false);
+            this.pnlNavegacion.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtmarca;
+        private System.Windows.Forms.TextBox txtmodelo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtyear;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel pnlNavegacion;
+        private System.Windows.Forms.Label lblregistroxden;
+        private System.Windows.Forms.Button btnUltimo;
+        private System.Windows.Forms.Button btnSiguiente;
+        private System.Windows.Forms.Button btnAnterior;
+        private System.Windows.Forms.Button btnPrimero;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblidvehiculo;
     }
 }
 
